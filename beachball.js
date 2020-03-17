554776dcdb0fb77e69947fb9160377620159c564
rachelvo <rach0727@stud.kea.dk>
2020-03-17 14:06:34 +0100

    somewhat done with 1st exercise

--- beachball.js
+++ beachball.js
@@ -1,4 +1,14 @@
 "use strict";
 import { gsap } from "gsap";
 const ball = document.querySelector(".ball");
+
 gsap.to(ball, { transform: "translateX(400px)", duration: 2 });
+gsap.to(ball, {
+  transform: " translateX(400px) rotate(200deg)",
+  duration: 2
+});
+gsap.to(ball, {
+  transform: " translateX(400px) translateY(100px) rotateZ(200deg) scale(0)",
+  duration: 2,
+  delay: 1
+});
